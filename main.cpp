9cb557e71d7ec10b1723eafef03b4262bf06d98b
Leanne Dong <ldon@staff.concordia.ca>
2020-08-04 23:44:13 -0400

    update

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include "graph.h"
+
+template <class ForwardIterator>
+void print_cycles(ForwardIterator first, ForwardIterator last);
+
+int main()
+{
+    undirected::Graph g(11);
+    //Graph g(11);
+    g.addEdge(0, 1);
+    g.addEdge(0, 6);
+    g.addEdge(0, 7);
+    g.addEdge(1, 2);
+    g.addEdge(2, 3);
+    g.addEdge(2, 7);
+    g.addEdge(3, 4);
+    g.addEdge(4, 5);
+    g.addEdge(4, 8);
+    g.addEdge(5, 6);
+    g.addEdge(7, 8);
+    g.addEdge(8, 9);
+    g.addEdge(9, 10);
+    g.addEdge(10, 6);
+
+    std::vector<int> cycles;
+    g.Gotlieb(back_inserter(cycles));
+    print_cycles(begin(cycles), end(cycles));
+
+    g.printMat();
+}
+
+// Requires a sequence of closed cycles.
+template <class ForwardIterator>
+void print_cycles(ForwardIterator first, ForwardIterator last)
+{
+    using T = typename iterator_traits<ForwardIterator>::value_type;
+    while (first != last)
+    {
+        auto const cycle_start = first++;
+        first = ++find(first, last, *cycle_start);
+        copy(cycle_start, first, ostream_iterator<T>(cout, " "));
+        cout << "\n";
+    }
+}
